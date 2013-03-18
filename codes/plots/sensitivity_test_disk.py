796621da51a8a9978d13002f5b0ee23aafcb3f3e
Polytopia(trent) <akumano@reed.edu>
2013-03-18 00:41:01 -0700

    save parallelized sensitivity test

--- codes/plots/sensitivity_test_disk.py
+++ codes/plots/sensitivity_test_disk.py
@@ -1,6 +1,8 @@
-
 from shapes import *
 import pprocess as pproc
+import time
+
+eps = sp.finfo(float).eps
 
 def sensitivity_test_disk(d, R, delta, bound, numitr, ballnum):
     # d: The parameter for the inner circle
@@ -18,7 +20,7 @@ def sensitivity_test_disk(d, R, delta, bound, numitr, ballnum):
     result = []
     maxdist = 0
     eps = sp.finfo(float).eps
-    for b in sh.balls: # test sensitivity for each point # *PARALLELIZE THIS*
+    for b in sh.balls: # test sensitivity for each point
         other = rotation(b, -eps) # the other point is a point obtained by clockwise rotation by epsilon
         for i in range(numitr):
             nrm = sh.billard_mod()
@@ -40,42 +42,41 @@ def p_sensitivity_test(d, R, delta, bound, numitr, ballnum):
     sh = BasicShape('ray', d, 256)
     sh.billard_setup_disk(R, delta, ballnum)
     testpts = sh.balls # save the initial setup
-    result = pproc.Map(limit=limit)
-    p_billard = result.manage(pproc.MakeParallel(p_sensitivity_test_helper))
-    maxdist = 0
-    eps = sp.finfo(float).eps
+    result = pproc.Map(limit=10, reuse=1)
+    p_billard = result.manage(pproc.MakeReusable(p_sensitivity_test_helper))
     for b in sh.balls: # test sensitivity for each point
+        p_billard(sh, b, d, delta, bound, numitr)
+    
+    return result
 
-def p_sensitivity_test_helper(ball, d, R, delta, bound, numitr, ballnum):
-        other = rotation(b, -eps) # the other point is a point obtained by clockwise rotation by epsilon
+def p_sensitivity_test_helper(shape, ball, d, delta, bound, numitr):
+        other = rotation(ball, -eps) # the other point is a point obtained by clockwise rotation by epsilon
+        maxdist = 0
         for i in range(numitr):
-            nrm = sh.billard_mod()
+            nrm = shape.billard_mod()
             if nrm > maxdist:
                 maxdist = nrm
             if nrm > bound:
-                result.append(1)
+                return 1
         # otherwise, the system didn't show sensitive dependence on initial conditions.
-        result.append(0)
-    result = sp.array(result)
-    result = sp.transpose(sp.vstack((testpts[:,0],testpts[:,1],result)))
-    return result
+        return 0
 
 
-def run_test():
-    R = 10
-    dls = [0.5, 1.0, 1.5, 2.0]
-    delta = 0.02
-    ballnum = 200
-    eps = sp.finfo(float).eps
-    bound = 1
-    numitr=10000
-    #R = 2
-    #dls = [0.5]
-    #delta = 0.5
-    #ballnum = 4
+def run_test(parallel=False):
+    #R = 10
+    #dls = [0.5, 1.0, 1.5, 2.0]
+    #delta = 0.02
+    #ballnum = 200
     #eps = sp.finfo(float).eps
     #bound = 1
-    #numitr=200
+    #numitr=10000
+    R = 4
+    dls = [0.5,1.0]
+    delta = 0.2
+    ballnum = 4
+    eps = sp.finfo(float).eps
+    bound = 1
+    numitr=200
 
     f = open('sensitivity_test_disk_result.txt', 'w')
     f.write('R: ')
@@ -95,12 +96,22 @@ def run_test():
         f.write('d: ')
         f.write(str(d))
         f.write("\n")
-        result = sensitivity_test_disk(d, R, delta, bound, numitr, ballnum)
+        if parallel:
+            result = p_sensitivity_test(d, R, delta, bound, numitr, ballnum)
+        else:
+            result = sensitivity_test_disk(d, R, delta, bound, numitr, ballnum)
         for p in result:
             f.write(str(p))
             f.write("\n")
    
 
 # MAIN
+t = time.time()
+run_test(parallel=True)
+print("Parallel: ")
+print(str(time.time() - t))
+t = time.time()
 run_test()
-    
+print("Not parallel: ")
+print(str(time.time() - t))
+   
