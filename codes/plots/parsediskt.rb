ebbd79eab3df2b4f7f4e2c3b4b0bc58a3a8e2991
Atsuya Kumano <akumano@reed.edu>
2013-03-24 16:29:15 -0700

    strip transformation images done

---
+++ codes/plots/parsediskt.rb
@@ -0,0 +1,7 @@
+version = '9-10'
+f = 'disk_transformation_result' + version + '.txt'
+target = 'disk_transformation' + version + '.dat'
+command = "echo 'x y' > #{target}"
+system(command)
+command = 'cat ' + f + " | tail -n +6 >> #{target}"
+system(command)
--- codes/plots/transformation_analysis.R
+++ codes/plots/transformation_analysis.R
@@ -1,5 +1,7 @@
-dat = read.table('disk_transformation_1.dat',header=TRUE)
-
 library('ggplot2')
-image = ggplot(dat, aes(x = x, y = y)) + geom_point(shape=10, size=0.05,color='darkred')
-ggsave(file="disk_transformation_1.svg", plot=image)
+
+dat = read.table('disk_transformation4-5.dat',header=TRUE)
+print(
+      ggplot(dat, aes(x = x, y = y)) + geom_point(shape=10, size=0.05,color='darkred')
+      )
+#ggsave(file="disk_transformation_1.svg", plot=image)
